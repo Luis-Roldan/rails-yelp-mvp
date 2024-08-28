8760a33179369e8123c4210d60d29089b2678b0a
luis roldan <roldanluis022@gmail.com>
2024-08-28 17:06:46 -0500

    part1

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,40 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Restaurant.destroy_all
+
+Restaurant.create(
+  name: "Ohashi",
+  address: "Av brasil 2512",
+  phone_number: "+51 996842355",
+  category: "japanese"
+)
+
+Restaurant.create(
+  name: "Islandia",
+  address: "Av cuba 5124",
+  phone_number: "+51 975242363",
+  category: "chinese"
+)
+
+Restaurant.create(
+  name: "Corralito",
+  address: "Av brasil 3795",
+  phone_number: "+51 985747969",
+  category: "belgian"
+)
+
+Restaurant.create(
+  name: "Trattoria",
+  address: "Av Pardo 3651",
+  phone_number: "+51 978456123",
+  category: "italian"
+)
+
+Restaurant.create(
+  name: "Cremeria toscana",
+  address: "Av san Felipe 1452",
+  phone_number: "+51 968523147",
+  category: "french"
+)
